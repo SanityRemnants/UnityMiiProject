f761a6a6b0136a6b85ef06099e3f4fe110809956
Konrad Drozd <konraddrozd02@gmail.com>
2022-11-22 20:24:07 +0100

    Implemented animations, camera follow and score system
    Implemented player(idle, run, jump) as well as flipping the player sprite according to the movement directions and objective animations(idle).
    Implemented script that makes the camera follow player smoothly.
    Collecting bonus objects grants points(only visible through console).

--- 2D Platformer BSamujło KDrozd MJaroszewicz/Assets/Scripts/FoxControler.cs
+++ 2D Platformer BSamujło KDrozd MJaroszewicz/Assets/Scripts/FoxControler.cs
@@ -6,49 +6,84 @@ public class FoxControler : MonoBehaviour
 {
     [Range(0.01f, 20)][SerializeField] private float moveSpeed = 0.1f;
     private Rigidbody2D rigidBody;
+    private Animator animator;
+    private bool isWalking;
+    private bool isFacingRight=true;
+    private int score = 0;  
 
     public const float rayLength = 0.4f;
     public float jumpforce = 5.0f;
     public LayerMask groundLayer;
 
-    void Start()
+    private void Start()
     {
         
     }
 
-    void Update()
+    private void Update()
     {
+        isWalking = false;
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
+            isWalking = true;
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            if (!isFacingRight)
+            {
+                flip();
+            }
         } if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
+            isWalking = true;
             transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            if(isFacingRight)
+            {
+                flip();
+            }
         }if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
             jump();
         }
-
+        animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isGrounded", isGrounded());
         //Debug.DrawRay(transform.position, rayLength*Vector3.down, Color.white, 1, false);
     }
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    bool isGrounded()
+    private bool isGrounded()
     {
         return Physics2D.Raycast(this.transform.position, Vector2.down, rayLength, groundLayer.value);
     }
 
-    void jump()
+    private void jump()
     {
         if (isGrounded())
         {
             rigidBody.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
 
             Debug.Log("Jumping");
+        }
+    }
+
+    private void flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("bonus"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+            other.gameObject.SetActive(false);
         }
     }
 }
